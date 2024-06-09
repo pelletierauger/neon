848afa3e493a93c019e2acd7508bb4ff966d0cf4
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2024-06-09 03:04:30 -0400

    Make an animation

--- sketch.js
+++ sketch.js
@@ -63,73 +63,120 @@ function setup() {
 let ww;
 
 draw = function() {
-    ww = map(sin(frameCount * 0.05), -1, 1, 0.05, 1);
-    rectangles = 0;
-    let t = frameCount * 5;
-    let osc = 0.1;
+    // ww = map(sin(frameCount * 0.05), -1, 1, 0.05, 1);
+    // rectangles = 0;
+    // let t = frameCount * 5;
+    // let osc = 0.1;
+    // vertices = [];
+    // colors = [];
+    // indices = [];
+    // let rectangle;
+    // rectangle = makeQuad({
+    //     c: [0, 0, 0, 1],
+    //     v: [
+    //         [-2 + (Math.sin(t * 0.05) * osc), -2 + (Math.cos(t * 0.05) * osc)],
+    //         [2 + (Math.sin(t * 0.015) * osc), -2 + (Math.cos(t * 0.015) * osc)],
+    //         [2 + (Math.sin(t * 0.015) * osc), 2 + (Math.cos(t * 0.015) * osc)],
+    //         [-2 + (Math.sin(t * 0.05) * osc), 2 + (Math.cos(t * 0.05) * osc)]
+    //     ]
+    // });
+    // addRectangleToBuffers(rectangle);
+    // setShaders2();
+    // amountOfLines = 0;
+    // for (let i = 0; i < 10; i++) {
+    //     lineOptions.r = 0.5;
+    //     lineOptions.g = 0; lineOptions.b = 0;
+    //     lineOptions.a = 1;
+    //     lineOptions.weight = 0.001;
+    //     lineOptions.blurFactor = 0.075;
+    //     makeLine(0 - 1 + (i * 0.15), -0.2, 0.5 - 1 + (i * 0.15), 0.5);
+    //     amountOfLines++;
+    //     makeLine(0 - 1 + (i * 0.15), 0.2, 0.5 - 1 + (i * 0.15), -0.5);
+    //     amountOfLines++;
+    //     lineOptions.blurFactor = 0.005;
+    //             lineOptions.r = 1;
+    //     lineOptions.a = 1;
+    //     // amountOfLines = 0;
+    //     makeLine(0 - 1 + (i * 0.15), -0.2, 0.5 - 1 + (i * 0.15), 0.5);
+    //     amountOfLines++;
+    //     makeLine(0 - 1 + (i * 0.15), 0.2, 0.5 - 1 + (i * 0.15), -0.5);
+    //     amountOfLines++;
+    // }
+    // let posR = [0, 0, Math.cos(t), Math.sin(t)];
+    // let rotate = function(p, a) {
+    //     return [
+    //         p.x * a.y + p.y * a.x,
+    //         p.y * a.y - p.x * a.x
+    //     ];
+    // };
+    let testnew = true;
+if (testnew) {
+        let makeLine = function(x0, y0, x1, y1, w) {
+        let a0 = Math.atan2(y1 - y0, x1 - x0);
+        let halfPI = Math.PI * 0.5;
+        let c0 = Math.cos(a0 + halfPI) * w;
+        let c1 = Math.cos(a0 - halfPI) * w;
+        let s0 = Math.sin(a0 + halfPI) * w;
+        let s1 = Math.sin(a0 - halfPI) * w;
+        let xA = x0 + c0;
+        let yA = y0 + s0;
+        let xB = x0 + c1;
+        let yB = y0 + s1;
+        let xC = x1 + c0;
+        let yC = y1 + s0;
+        let xD = x1 + c1;
+        let yD = y1 + s1;
+        return [xA, yA, xB, yB, xC, yC, xD, yD];
+    };
+    let ii = [0, 1, 2, 0, 2, 3];
+    indices = [];
     vertices = [];
     colors = [];
-    indices = [];
-    let rectangle;
-    rectangle = makeQuad({
-        c: [0, 0, 0, 1],
-        v: [
-            [-2 + (Math.sin(t * 0.05) * osc), -2 + (Math.cos(t * 0.05) * osc)],
-            [2 + (Math.sin(t * 0.015) * osc), -2 + (Math.cos(t * 0.015) * osc)],
-            [2 + (Math.sin(t * 0.015) * osc), 2 + (Math.cos(t * 0.015) * osc)],
-            [-2 + (Math.sin(t * 0.05) * osc), 2 + (Math.cos(t * 0.05) * osc)]
-        ]
-    });
-    addRectangleToBuffers(rectangle);
-    setShaders2();
-    amountOfLines = 0;
-    for (let i = 0; i < 10; i++) {
-        lineOptions.r = 0.5;
-        lineOptions.g = 0; lineOptions.b = 0;
-        lineOptions.a = 1;
-        lineOptions.weight = 0.001;
-        lineOptions.blurFactor = 0.075;
-        makeLine(0 - 1 + (i * 0.15), -0.2, 0.5 - 1 + (i * 0.15), 0.5);
-        amountOfLines++;
-        makeLine(0 - 1 + (i * 0.15), 0.2, 0.5 - 1 + (i * 0.15), -0.5);
-        amountOfLines++;
-        lineOptions.blurFactor = 0.005;
-                lineOptions.r = 1;
-        lineOptions.a = 1;
-        // amountOfLines = 0;
-        makeLine(0 - 1 + (i * 0.15), -0.2, 0.5 - 1 + (i * 0.15), 0.5);
-        amountOfLines++;
-        makeLine(0 - 1 + (i * 0.15), 0.2, 0.5 - 1 + (i * 0.15), -0.5);
-        amountOfLines++;
+    for (let j = 0; j < 50; j++) {
+        for (let k = 0; k < ii.length; k++) {
+            indices.push(ii[k] + (j*4));
+        }
+        let nj = Math.PI * 2 / 50 * j;
+        let x1 = -0.25 + Math.cos(nj + frameCount * 1e-2) * 0.5;
+        let y1 = -0.25 + Math.sin(nj + frameCount * 1e-2) * 0.5;
+        let x0 = x1 + 0.5;
+        let y0 = y1 + 0.5;
+        // x1 = 0.5;
+        // y1 = 0;
+        let ml = makeLine(x0, y0, x1, y1, 0.0625);
+        let vv = [
+            ml[0], ml[1], 0, 
+            ml[2], ml[3], 0,
+            ml[6], ml[7], 0,
+            ml[4], ml[5], 0,
+        ];
+        for (let k = 0; k < vv.length; k++) {
+            vertices.push(vv[k]);
+        }
+        let a = Math.sin(nj - frameCount * -1e-1) * 0.5 + 0.5;
+        let cc = [
+            0, 0, 0, a, 
+            1, 0, 0, a, 
+            1, 0, 1, a, 
+            0, 0, 1, a,
+        ];
+        for (let k = 0; k < cc.length; k++) {
+            colors.push(cc[k]);
+        }
     }
-    let posR = [0, 0, Math.cos(t), Math.sin(t)];
-    let rotate = function(p, a) {
+}
+        let rotate = function(p, a) {
         return [
             p.x * a.y + p.y * a.x,
             p.y * a.y - p.x * a.x
         ];
     };
-    let testnew = true;
-    if (testnew) {
-        indices = [0, 1, 2, 0, 2, 3];
-        vertices = [
-        0,   0,   0, 
-        0,   0.75, 0, 
-        0.125, 0.75, 0, 
-        0.125, 0,   0];
-        let rotateAngle = {x: Math.cos(t * 0.005), y: Math.sin(t * 0.005)};
+            let rotateAngle = {x: Math.cos(frameCount * 1e-2), y: Math.sin(frameCount * 1e-2)};
         for (let i = 0; i < vertices.length; i += 3) {
             let newPos = rotate({x: vertices[i], y: vertices[i+1]}, rotateAngle);
             vertices[i] = newPos[0];
             vertices[i+1] = newPos[1];
         }
-        colors = [
-        0, 0, 0, 1, 
-        0, 0, 1, 1, 
-        1, 0, 1, 1, 
-        1, 0, 0, 1,
-        ];
-    }
     var vertex_buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
