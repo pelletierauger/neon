04be6e91a49e0bac8ba37427ee06873bba917236
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2024-09-01 23:04:13 -0400

    Make the sphere shimmer in the evening sky

--- shaders.js
+++ shaders.js
@@ -569,6 +569,7 @@ smoothLine3D.vertText = `
     varying vec2 wh;
     varying float t;
     varying vec3 posUnit;
+    varying vec3 posUnit2;
     float map(float value, float min1, float max1, float min2, float max2) {
         return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
     }
@@ -615,6 +616,7 @@ smoothLine3D.vertText = `
         vec2 pos = vec2(0., 0.);
         vec4 pos0 = vec4(coordinatesA, 1.);
         vec4 pos1 = vec4(coordinatesB, 1.);
+        posUnit = pos0.xyz;
         // pos0.xyz *= map(sin(time *1e-1+pos0.y*2.), -1., 1., 0.95, 1.0);
         // pos1.xyz *= map(sin(time *1e-1+pos1.y*2.), -1., 1., 0.95, 1.0);
         // pos0.xyz *= 0.15
@@ -649,7 +651,7 @@ smoothLine3D.vertText = `
         c = color;
         uvs = uv;
         t = time;
-        posUnit = pos0.xyz;
+        posUnit2 = pos0.xyz;
     }
     // endGLSL
 `;
@@ -661,6 +663,7 @@ smoothLine3D.fragText = `
     varying vec2 wh;
     varying float t;
     varying vec3 posUnit;
+    varying vec3 posUnit2;
     float rand(vec2 co){
         return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(co.x)));
     }
@@ -690,8 +693,8 @@ smoothLine3D.fragText = `
         // gl_FragColor.a = min(1., gl_FragColor.a + pow(col, 2.) *  0.25);
         // gl_FragColor.rgb = gl_FragColor.gbr;
         // gl_FragColor.rgb = vec3(0.);
-        gl_FragColor.a *= 1.0-posUnit.z*0.35;
-        // gl_FragColor.rgb *= sin(posUnit.y*5.-t*0.5e-1)*0.5+0.5;
+        gl_FragColor.a *= 1.0-posUnit2.z*0.35;
+        gl_FragColor.rgb *= sin(posUnit.y*2.-t*0.5e-1)*0.5+0.5;
     }
     // endGLSL
 `;
@@ -1034,6 +1037,7 @@ smoothDots3D.vertText = `
     uniform vec2 resolution;
     varying float t;
     varying vec3 posUnit;
+    varying vec3 posUnit2;
     float map(float value, float min1, float max1, float min2, float max2) {
         return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
     }
@@ -1075,6 +1079,7 @@ smoothDots3D.vertText = `
         // pos = translate(0.0, 0., 0.5) * yRotate(time*2e-2) * xRotate(time*2e-2) * translate(0.0, 0., -0.5) * pos;
         // pos.xyz *= map(sin(time *1e-1+pos.y*2.), -1., 1., 0.95, 1.0);
         // pos.xyz *= 1.25;
+        posUnit = pos.xyz;
         pos = yRotate(-time*0.25e-2) * pos;
         pos = xRotate(time*0.25e-2) * pos;
         // pos = xRotate(-time*0.5e-2) * pos;
@@ -1085,8 +1090,8 @@ smoothDots3D.vertText = `
         gl_Position = vec4(pos.x, pos.y, 0.0, pos.z);
         gl_PointSize = 8.;
         t = time;
-        posUnit = pos.xyz;
         // gl_PointSize += (sin((length(coordinates*20.)*0.2-time*2e-1))*0.5+0.5)*14.;
+        posUnit2 = pos.xyz;
     }
     // endGLSL
 `;
@@ -1096,6 +1101,7 @@ smoothDots3D.fragText = `
     // uniform float time;
     varying float t;
     varying vec3 posUnit;
+    varying vec3 posUnit2;
     float rand(vec2 co){
         return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453 * (2.0 + sin(co.x)));
     }
@@ -1113,9 +1119,9 @@ smoothDots3D.fragText = `
         l = pow(l, 3.);
         float noise = rand(pos - vec2(cos(t), sin(t))) * 0.0625;
         gl_FragColor = vec4(vec3(1.0, pow(l, 2.)*0.25, 0.25), (l+halo-noise)*0.5);
-        gl_FragColor.rgb = vec3(0.0);
-        gl_FragColor.a *= 1.0-posUnit.z*0.3;
-        // gl_FragColor.rgb *= sin(posUnit.y*5.-t*0.5e-1)*0.5+0.5;
+        // gl_FragColor.rgb = vec3(0.0);
+        gl_FragColor.a *= 1.0-posUnit2.z*0.3;
+        gl_FragColor.rgb *= sin(posUnit.y*5.-t*0.5e-1)*0.5+0.5;
     }
     // endGLSL
 `;
